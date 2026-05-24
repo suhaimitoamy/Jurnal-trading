736e67270139e754a62ac8f957df65252ddf5c76
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-05-24 08:34:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Jurnal-trading/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
